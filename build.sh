167ff534e391739230c130f4b0d7694f05b52f96
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-05-22 23:32:14 -0700

    templates and content added

---
+++ build.sh
@@ -0,0 +1,17 @@
+top = open('templates/top.html').read()
+content = open('content/index.html').read()
+bottom = open('templates/bottom.html').read()
+
+indexpage = top + content + bottom
+
+top = open('templates/top.html').read()
+content = open('content/resume.html').read()
+bottom = open('templates/bottom.html').read()
+
+resume = top + content + bottom
+
+top = open('templates/top.html').read()
+content = open('content/contact.html').read()
+bottom = open('templates/bottom.html').read()
+
+contact = top + content + bottom
