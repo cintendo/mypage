40766ccc1f35e6fe0b9b3aafd81b4e24b9df2631
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-05-23 15:12:27 -0700

    modified build.py

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
-# First, get the template files
+# Get Template Files
 top_template = open('templates/top.html').read()
 bottom_template = open('templates/bottom.html').read()
 
-# Read in index HTML code
+# readin index.html
 content = open('content/index.html').read()
 
-# Combine template HTML code with index HTML code
+#for index html
 index_html = top_template + content + bottom_template
 open('index.html', 'w+').write(index_html)
 
-# Rinse and repeat, but with resume HTML code
+# for resume html
 content = open('content/resume.html').read()
 resume_html = top_template + content + bottom_template
 open('resume.html', 'w+').write(resume_html)
 
-# And again, this time with contact HTML code
+# for contact html
 content = open('content/contact.html').read()
 contact_html = top_template + content + bottom_template
 open('contact.html', 'w+').write(contact_html)
