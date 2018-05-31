953d120d38e4a7ce2dbc653220ea37edbd516ad6
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-05-30 17:45:26 -0700

    updated templates, removed bash script, modified python sript

--- build.py
+++ build.py
@@ -1,20 +1,51 @@
-# First, get the template files
-top_template = open('templates/top.html').read()
-bottom_template = open('templates/bottom.html').read()
+pages = [
+    {
+        'filename': 'content/index.html',
+        'title': 'index',
+    },
+    {
+        'filename': 'content/resume.html',
+        'title': 'resume',
+    },
+    {
+        'filename': 'content/contact.html',
+        'title': 'contact',
+    },
+]
 
-# Read in index HTML code
-content = open('content/index.html').read()
+def main():
+    template = open('templates/base.html').read()
 
-# Combine template HTML code with index HTML code
-index_html = top_template + content + bottom_template
-open('index.html', 'w+').write(index_html)
 
-# Rinse and repeat, but with resume HTML code
-content = open('content/resume.html').read()
-resume_html = top_template + content + bottom_template
-open('resume.html', 'w+').write(resume_html)
+    for page in pages:
+        content = open(page['filename']).read()
+        full_page_contents = template.replace('{{content}}', content)
+        full_page = str('docs/'+ page['title'] + '.html')
+        open(full_page, 'w+').write(full_page_contents)
 
-# And again, this time with contact HTML code
-content = open('content/contact.html').read()
-contact_html = top_template + content + bottom_template
-open('contact.html', 'w+').write(contact_html)
+
+
+main()
+
+#
+#
+# # Read in index HTML code
+# content = open('content/index.html').read()
+#
+# # Combine template HTML code with index HTML code
+# index_html = top_template + content + bottom_template
+# open('index.html', 'w+').write(index_html)
+#
+# # Rinse and repeat, but with resume HTML code
+# content = open('content/resume.html').read()
+# resume_html = top_template + content + bottom_template
+# open('resume.html', 'w+').write(resume_html)
+#
+# # And again, this time with contact HTML code
+# content = open('content/contact.html').read()
+# contact_html = top_template + content + bottom_template
+# open('contact.html', 'w+').write(contact_html)
+#
+#
+if __name__ == '__main__':
+    main()
